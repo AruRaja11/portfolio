0bcd55c65a84ee7ff5ca4396f29392b20409b133
Arun Raja <147836952+AruRaja11@users.noreply.github.com>
2025-08-26 10:02:46 +0530

    Delete gunicorn_conf.py

--- gunicorn_conf.py
+++
@@ -1,4 +0,0 @@
-# gunicorn_conf.py
-# Use the uvicorn worker class, which is designed for async applications like FastAPI.
-workers = 1  # For the free tier, a single worker is sufficient and recommended.
-worker_class = "uvicorn.workers.UvicornWorker"
